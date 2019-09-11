e03ac955d7f8d5bafa3c42404089fbc91aa41283
Tasosan <tasosanastasiades@gmail.com>
2019-09-11 20:28:10 +0300

    Second Client notification

--- SignalR2/Pages/Index.cshtml.cs
+++ SignalR2/Pages/Index.cshtml.cs
@@ -2,16 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LearningSignalR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.SignalR;
 
 namespace SignalR2.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IHubContext<LearningHub> hubContext;
+
+        public IndexModel(IHubContext<LearningHub> hubContext)
+        {
+            this.hubContext = hubContext;
+        }
+
         public void OnGet()
         {
-
+            hubContext.Clients.All.SendAsync("ReceiveMessage", "Client connected to a secondary web application");
         }
     }
 }
